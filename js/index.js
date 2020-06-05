d892ea1f416f57f5185c82ae145109e759ce4752
Hug0Albert0 <hugorivera.cool.player@gmail.com>
2020-06-05 00:41:47 -0500

    Logica de respuesta correcta

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
-let contador = 0;
+//let contador = 0;
+let numeroCorrecto = Math.floor(Math.random() * 100) + 1;
+console.log(numeroCorrecto);
 document.getElementById("btnEnviar").addEventListener(
    "click",
    () => {
+      /*
       contador++;
       console.log(contador);
+      */
+      let valorRespuesta = document.getElementById("respuesta").value;
+      if(valorRespuesta) {
+         console.log(valorRespuesta);
+         //Hacer condicionales if - else para determinar si la respuesta es menor, mayor o igual que numeroCorrecto
+      } else{
+         console.log("No has ingresado ninguna respuesta. Intenta otra vez");
+      }
    }
 );
