aa2e65730724cfc9c671e6f446b88f3d1303372f
willowy <vazquezjoshua47@gmail.com>
2020-06-05 01:44:33 -0500

    Asi nomas quedo

--- js/index.js
+++ js/index.js
@@ -11,9 +11,24 @@ document.getElementById("btnEnviar").addEventListener(
       let valorRespuesta = document.getElementById("respuesta").value;
       if(valorRespuesta) {
          console.log(valorRespuesta);
+         if(isNaN( valorRespuesta)){
+            if (numeroCorrecto > valorRespuesta){
+               console.log("¡Fallaste, el numero es mayor a la respuesta correcta!");
+            } else if(numeroCorrecto < valorRespuesta){
+               console.log("¡Fallaste el numero es menor a la respuesta correcta!")
+            }
+            else{
+               console.log("Felicidades, has ganado!");
+            }
+         }
+         else{
+            ("No es un valor numerico lo siento :(")
+            return
+         }
+         
          //Hacer condicionales if - else para determinar si la respuesta es menor, mayor o igual que numeroCorrecto
       } else{
-         console.log("No has ingresado ninguna respuesta. Intenta otra vez");
+         console.log("Aun no ingresas un numero, intentalo de nuevo :(");
       }
    }
 );
