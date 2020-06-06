528e2849a6c4ba8a835d4210f969d3abb3c8b8a6
Hug0Albert0 <hugorivera.cool.player@gmail.com>
2020-06-06 02:39:33 -0500

    Se mofidica el look and feel del proyecto usando material design lite, se corrigen bugs de logica para intentos y condiciones de juego, se agrega link a twemoji para usar emojis

--- js/index.js
+++ js/index.js
@@ -1,49 +1,50 @@
-//let contador = 0;
+let intentos = 3;
 let numeroCorrecto = Math.floor(Math.random() * 100) + 1;
 console.log(numeroCorrecto);
 document.getElementById("btnEnviar").addEventListener(
    "click",
    () => {
-      /*
-      contador++;
-      console.log(contador);
-      */
-
-
-      let min = 1;
-      let max = 100;
-      var valorRespuesta;
-
-
-      for(var intentos = 0; intentos < 3 ; intentos++ ){
-         let valorRespuesta = document.getElementById("respuesta").value;
-         
-
-          if(valorRespuesta >= min && valorRespuesta <= max){
-            console.log(intentos)
-
-            if (numeroCorrecto > valorRespuesta){
-               console.log("¡Fallaste, el numero es mayor a la respuesta correcta!");
-            } 
-            else if(numeroCorrecto < valorRespuesta){
-               console.log("¡Fallaste el numero es menor a la respuesta correcta!");
-            }
-            else if(numeroCorrecto == valorRespuesta){
-               console.log("Felicidades, has ganado!");
-            }
-
-         }else{
-            console.log("Ingrese un valor valido");
-         }
-      }
-      valorRespuesta
-
-      if(valorRespuesta == numeroCorrecto){
-         alert("Felicidades adivinaste el numero en " +intentos+ " intentos.");
-      } else {
-         console.log("¡Te has terminado tus intentos, vuelve a intentarlo!");
-      }
-
+      fnJugar();
    }
-   );
-
+);
+document.onkeydown = (evento) => {
+   if (evento.keyCode == "13") {
+      fnJugar();
+   }
+}
+const fnJugar = () => {
+   if (intentos > 0) {
+      let valorRespuesta = document.getElementById("respuesta").value;
+      if (valorRespuesta) {
+         if(!isNaN(Number(valorRespuesta))) {
+            if (valorRespuesta > numeroCorrecto){
+               intentos--;
+               console.log(`
+                  ¡Fallaste, el numero es mayor a la respuesta correcta!
+                  Te quedan ${intentos} intento(s).
+               `);
+               document.getElementById("respuesta").value = "";
+            }
+            else if(valorRespuesta < numeroCorrecto){
+               intentos--;
+               console.log(`
+                  ¡Fallaste el numero es menor a la respuesta correcta!
+                  Te quedan ${intentos} intento(s).
+               `);
+               document.getElementById("respuesta").value = "";
+            }
+            else {
+               console.log("Felicidades adivinaste el numero");
+            }
+         } else {
+            console.log("Ingresa un número entero del 1 al 100");
+            document.getElementById("respuesta").value = "";
+         }
+      } else {
+         console.log("Ingresa un número entero del 1 al 100");
+         document.getElementById("respuesta").value = "";
+      }
+   } else {
+      console.log("Has perdido, ya no te quedan intentos");
+   }
+}
