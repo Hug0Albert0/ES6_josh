2a372432e4a356802c63bc7164ae9eb8f0df4818
Hugo AlbertoX Rivera Diaz <hugo.albertox.rivera.diaz@intel.com>
2020-06-06 23:05:13 -0500

    Cambios nuevos

--- js/index.js
+++ js/index.js
@@ -23,36 +23,34 @@ document.onkeydown = (evento) => {
 }
 
 const fnJugar = () => {
-   if (intentos > 0) {
-      let valorRespuesta = inputRespuesta.value;
-      if (valorRespuesta) {
-         if(!isNaN(Number(valorRespuesta))) {
-            if (valorRespuesta >= 1 && valorRespuesta <= 100) {
-               if (valorRespuesta > numeroCorrecto){
-                  intentos--;
-                  mensaje = `
-                     <h4>¡Fallaste, el numero es mayor a la respuesta correcta!
-                     Te quedan ${intentos} intento(s).</h4>
-                  `;
-                  contenedorMensaje.innerHTML = mensaje;
-                  inputRespuesta.value = "";
-               }
-               else if(valorRespuesta < numeroCorrecto){
-                  intentos--;
-                  mensaje = `
-                     <h4>¡Fallaste el numero es menor a la respuesta correcta!
-                     Te quedan ${intentos} intento(s).</h4>
-                  `;
-                  contenedorMensaje.innerHTML = mensaje;
-                  inputRespuesta.value = "";
-               }
-               else {
-                  mensaje = `<h4>Felicidades adivinaste el numero: ${numeroCorrecto}</h4>`;
-                  contenedorMensaje.innerHTML = mensaje;
-                  inputRespuesta.value = "";
-               }
-            } else {
-               fnError();
+   let valorRespuesta = inputRespuesta.value;
+   if (valorRespuesta) {
+      if(!isNaN(Number(valorRespuesta))) {
+         if (valorRespuesta >= 1 && valorRespuesta <= 100) {
+            if (valorRespuesta > numeroCorrecto){
+              intentos--;
+               mensaje = `
+                  <h4>¡Fallaste, el numero es mayor a la respuesta correcta!
+                  Te quedan ${intentos} intento(s).</h4>
+               `;
+               contenedorMensaje.innerHTML = mensaje;
+               inputRespuesta.value = "";
+               fnComprobarIntentos();
+            }
+            else if(valorRespuesta < numeroCorrecto){
+              intentos--;
+               mensaje = `
+                  <h4>¡Fallaste el numero es menor a la respuesta correcta!
+                  Te quedan ${intentos} intento(s).</h4>
+               `;
+               contenedorMensaje.innerHTML = mensaje;
+               inputRespuesta.value = "";
+               fnComprobarIntentos();
+            }
+            else {
+               mensaje = `<h4>Felicidades adivinaste el numero: ${numeroCorrecto}</h4>`;
+               contenedorMensaje.innerHTML = mensaje;
+               inputRespuesta.value = "";
             }
          } else {
             fnError();
@@ -61,9 +59,7 @@ const fnJugar = () => {
          fnError();
       }
    } else {
-      mensaje = `<h4>Has perdido, ya no te quedan intentos</h4>`;
-      contenedorMensaje.innerHTML = mensaje;
-      inputRespuesta.value = "";
+      fnError();
    }
 }
 
@@ -71,4 +67,16 @@ const fnError = () => {
    mensaje = `<h4>Ingresa un número entero del 1 al 100</h4>`;
    contenedorMensaje.innerHTML = mensaje;
    inputRespuesta.value = "";
+}
+
+const fnTerminaTurno = () => {
+   mensaje = `<h4>Has perdido, ya no te quedan intentos</h4>`;
+   contenedorMensaje.innerHTML = mensaje;
+   inputRespuesta.value = "";
+}
+
+const fnComprobarIntentos = () => {
+   if(intentos == 0) {
+      fnTerminaTurno();
+   }
 }
