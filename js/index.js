08d0a84643dd42fca0065f0a908cfb60a32dd0f5
willowy <vazquezjoshua47@gmail.com>
2020-06-07 00:26:43 -0500

    Se añadieron emojis

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ let btnEnviar = document.getElementById("btnEnviar");
 const fnCargarJuego = () => {
    intentos = 3;
    numeroCorrecto = Math.floor(Math.random() * 100) + 1;
+   console.log(numeroCorrecto)
    contenedorMensaje.innerHTML = "";
    inputRespuesta.value = "";
    btnReiniciar.classList.add("hidden");
@@ -42,7 +43,7 @@ const fnJugar = () => {
             if (valorRespuesta > numeroCorrecto){
               intentos--;
                mensaje = `
-                  <h4>¡Fallaste!</h4>
+                  <h4>¡Fallaste! <span>&#x1F613;</span>  <span>&#x1F446;</span>  <span>&#x1F446;</span> </h4>
                   <h5>El número es mayor a la respuesta correcta!
                   Te quedan ${intentos} intento(s).</h5>
                `;
@@ -53,7 +54,7 @@ const fnJugar = () => {
             else if(valorRespuesta < numeroCorrecto){
               intentos--;
                mensaje = `
-                  <h4>¡Fallaste!</h4>
+                  <h4>¡Fallaste!<span>&#x1F613;</span> <span>&#x1F447;</span> <span>&#x1F447;</span> </h4>
                   <h5>El número es menor a la respuesta correcta!
                   Te quedan ${intentos} intento(s).</h5>
                `;
@@ -63,7 +64,7 @@ const fnJugar = () => {
             }
             else {
                mensaje = `
-                  <h4>¡¡¡Felicidades!!!</h4>
+                  <h4><span>&#x1F389;</span>¡¡¡Felicidades!!! <span>&#x1F973;</span> <span>&#x1F3C6;</span> <span>&#x1F389;</span></h4>
                   <h5> Has adivinado, tu número de la suerte es: ${numeroCorrecto}!!!</h5>`;
                contenedorMensaje.innerHTML = mensaje;
                inputRespuesta.value = "";
@@ -83,15 +84,15 @@ const fnJugar = () => {
 }
 
 const fnError = () => {
-   mensaje = `<h4>Ingresa un número entero del 1 al 100</h4>`;
+   mensaje = `<h4>Ingresa un número entero del 1 al 100  <span>&#x1F62C;</span>	.</h4>`;
    contenedorMensaje.innerHTML = mensaje;
    inputRespuesta.value = "";
 }
 
 const fnTerminaTurno = () => {
    mensaje = `
-      <h4>Has perdido</h4>
-      <h5>Ya no te quedan intentos</h5>`;
+      <h4>¡Has perdido! <span>&#x1F62D;</span> <span>&#x1F62D;</span> <span>&#x1F62D;</span>	</h4>
+      <h5>Ya no te quedan intentos :(( .</h5>`;
    contenedorMensaje.innerHTML = mensaje;
    inputRespuesta.value = "";
    btnReiniciar.classList.remove("hidden");
